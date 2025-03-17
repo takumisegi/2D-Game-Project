f71692d9cde8416741c2aeb6cd8c5e0070ed2c87
TaoYuanyue <ytaomica@gmail.com>
2025-03-17 13:40:16 -0400

    fix shark direction

---
+++ 2d-game-project/Scene/shark_left_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const Speed = 100
+
+var direction = 1
+
+@onready var ray_cast_right = $"RayCast right"
+@onready var ray_cast_left = $"RayCast left"
+@onready var shack_sprite = $"DeepSeaPanic-assets-shark-v01"
+
+func _process(delta):
+	if ray_cast_left.is_colliding():
+		direction = 1
+		shack_sprite.flip_h = true
+	if ray_cast_right.is_colliding():
+		direction = -1
+		shack_sprite.flip_h = false
+		
+	position.x += direction * Speed * delta
+		
+		
+	move_and_slide()
