8f58e138ce239452ae0e4b12458425168e2321d5
takumisegi <takumi.segi@gmail.com>
2025-03-17 11:31:23 -0400

    Win Screen - load on diamond

---
+++ 2d-game-project/deep_sea_panic_assets_treasure_v_01.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+var entered = false
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	entered = true
+	print("diamond!")
+	queue_free()
+	get_tree().change_scene_to_file("res://Scene/Win screen.tscn")
+	
+
+func _on_area_2d_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	entered = false
+	
+	
