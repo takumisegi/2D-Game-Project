ddb24e5d9271477d9ccb07b3cff12f73a1768ae4
TaoYuanyue <ytaomica@gmail.com>
2025-03-23 20:44:37 -0400

    1

--- 2d-game-project/Scripts/enemy_fish.gd
+++ 2d-game-project/Scripts/enemy_fish.gd
@@ -1,18 +1,6 @@
-extends CharacterBody2D
+extends 
 
-@onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
-@export var target_to_chase : CharacterBody2D 
-
-const SPEED = 70.0
-func _ready() -> void:
-	navigation_agent.target_position = target_to_chase.global_position
-func _physics_process(delta: float) -> void:
-	navigation_agent.target_position = target_to_chase.position
-	velocity = navigation_agent.get_next_path_position() - position
-	velocity *= SPEED
-	
-	#velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * SPEED
-	 
-
+export var speed = 100
+var 
 
 	move_and_slide()
